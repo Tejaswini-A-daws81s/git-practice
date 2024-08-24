5ea81006561155476c6d5dfc91a00c2a2fd2ad17
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-24 15:01:38 +0530

    Entering Username and Passwor

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your username"
+
+read USERNAME     #to get input from user
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password"
+
+read PASSWORD
+
+echo "Password entered is: $PASSWORD"
