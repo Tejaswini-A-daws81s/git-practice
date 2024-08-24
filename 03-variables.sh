d4626557d336778219f3816754f11eaf80c94c3c
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-24 14:43:29 +0530

    passing arguements

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi ${PERSON2}, how are you"
+echo "${PERSON2}:: I am fine $PERSON1, how about you"
+echo "$PERSON1:: I am great, how are things going, all good?"
+echo "${PERSON2}:: Yeah!, Recently i joined devops course"
