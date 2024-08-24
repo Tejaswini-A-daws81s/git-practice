dcdd4cae07678b8e6071bf1d410490b16d394607
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-24 14:09:52 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$PERSON1:: Hi {$PERSON2}, how are you"
-echo "{$PERSON2}:: I am fine $PERSON1, how about you"
+echo "$PERSON1:: Hi ${PERSON2}, how are you"
+echo "${PERSON2}:: I am fine $PERSON1, how about you"
 echo "$PERSON1:: I am great, how are things going, all good?"
-echo "{$PERSON2}:: Yeah!, Recently i joined devops course"
+echo "${PERSON2}:: Yeah!, Recently i joined devops course"
