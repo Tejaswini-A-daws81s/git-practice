6a035212efadf1af3476eed6bf098625ba9e0bef
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-24 15:20:09 +0530

    Array

--- 05-array.sh
+++ 05-array.sh
@@ -3,8 +3,8 @@
 FRUITS=("APPLE" "ORANGE" "STRAWBERRY")   #ARRAY
 
 #index starts from 0, size starts from 1, total size is 3, while printing print fruit index
-echo "First Fruit is: ${FRUIT[0]}"
-echo "Second Fruit is: ${FRUIT[1]}"
-echo "Third Fruit is: ${FRUIT[2]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
 
-echo "Three Fruits which are entered is: ${FRUIT[@]}"
+echo "Three Fruits which are entered is: ${FRUITS[@]}"
