78f026ee0a2d3f7359efa15ccea39856d1819854
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-24 13:56:56 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, how are you"
+echo "Suresh:: I am fine Ramesh, how about you"
+echo "Ramesh:: I am great, how are things going, all good?"
+echo "Suresh:: Yeah!, Recently i joined devops course"
