55ed8a15a9063b0a2039115960861f33a83ff197
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-24 14:04:51 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi {$PERSON2}, how are you"
+echo "{$PERSON2}:: I am fine $PERSON1, how about you"
+echo "$PERSON1:: I am great, how are things going, all good?"
+echo "{$PERSON2}:: Yeah!, Recently i joined devops course"
