312eff610bbc300b754f0ba7c0561fe703f4a20d
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-27 18:08:18 +0530

    Array

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -4,7 +4,7 @@ echo "All variables: $@"
 echo "The number of variables passed to the script: $#"
 echo "The name of the script is: $0"
 echo "Current working directory is: $PWD"
-echo "Home directory of the current user: $home"
+echo "Home directory of the current user: $HOME"
 echo "Process ID of the current shell script: $$"
 sleep 100 &
 echo "Process id of last background command: $!"
