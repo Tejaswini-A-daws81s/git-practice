f6377c1bb4635d6a2b215f0a950baefa29d1d93d
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-08-31 15:08:25 +0530

    shell

--- 24-redirectors.sh
+++ 24-redirectors.sh
@@ -31,8 +31,18 @@ VALIDATE(){
     fi
 }
 
+USAGE(){
+     echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
+     exit 1
+}
+
 
 CHECK_ROOT
+
+if [ $# -eq 0 ]
+then 
+  USAGE
+fi  
 
 for package in $@
 do
